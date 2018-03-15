174f0f142ca59691180e703934fe38ff1d99c1db
Caroline Vurlumis <cvurlumis@gmail.com>
2018-03-14 22:06:23 -0700

    adding static pages and links

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+  def products
+  end
+
+  def about
+  end
+
+  def takeAction
+  end
+
+  def cart
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  get 'home/products'
+
+  get 'home/about'
+
+  get 'home/takeAction'
+
+  get 'home/cart'
+
+  get 'home/contact'
+
+  root 'home#products'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
