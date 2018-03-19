7bf032caf2168dcff20b38e4aab3a543f85903d0
Caroline Vurlumis <cvurlumis@gmail.com>
2018-03-18 17:10:37 -0700

    added nav bar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,26 @@
 class HomeController < ApplicationController
+
+  def index
+    @current_page='index'
+  end
+
   def products
+    @current_page='products'
   end
 
   def about
+    @current_page='about'
   end
 
   def takeAction
+    @current_page='takeAction'
   end
 
   def cart
+    @current_page='cart'
   end
 
   def contact
+    @current_page='contact'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+   # resources :home
+
   get 'home/products'
 
   get 'home/about'
@@ -9,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'home/contact'
 
-  root 'home#products'
+  get 'home/index'
+
+  root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
