5a9f0aa7691cbd729f9a7e14a5bbf2930b982cf3
Caroline Vurlumis <cvurlumis@gmail.com>
2018-05-23 21:24:12 -0700

    Did we fix it?

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :order_items
+  has_many :order_items, dependent: destroy
 
   default_scope { where(active: true) } #checks if active flag is set to true. Ensures that deleted/inactive products aren't shown
 end
--- db/migrate/20160321165946_update_foreign_key.rb
+++ db/migrate/20160321165946_update_foreign_key.rb
@@ -1,9 +1,3 @@
 class UpdateForeignKey < ActiveRecord::Migration[5.1]
-  def change
-    # remove the old foreign_key
-    remove_foreign_key :product, :order
 
-    # add the new foreign_key
-    add_foreign_key :product, :order, on_delete: :cascade
-  end
 end
