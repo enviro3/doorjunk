8aa74e6a3724508f859426f711f92616ac95f23f
Caroline Vurlumis <cvurlumis@gmail.com>
2018-05-23 21:54:29 -0700

    Revert "Fix foreign keys maybe?"
    This reverts commit 979f7be5a05690e5660e97c3d23b0b1e10dac57e.

--- db/migrate/20180524045036_fix_foreign_keys_maybe.rb
+++
@@ -1,8 +0,0 @@
-class FixForeignKeysMaybe < ActiveRecord::Migration[5.1]
-  def change
-    remove_foreign_key :order_items, :product
-    remove_foreign_key :order_items, :order
-    add_foreign_key :order_items, :product, on_delete: :cascade
-    add_foreign_key :order_items, :order, on_delete: :cascade
-  end
-end
