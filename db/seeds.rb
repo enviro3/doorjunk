a204b17b9101484a6dd6c4dc7ba66f8e4f5f5e3d
Caroline Vurlumis <cvurlumis@gmail.com>
2018-05-23 21:12:48 -0700

    changed sticker to 50 c

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ Product.create! id: 3, name: "Sticker #3", price: 2.50, active: true
 Product.create! id: 4, name: "Sticker #4", price: 2.50, active: true
 Product.create! id: 5, name: "Sticker #5", price: 2.50, active: true
 Product.create! id: 6, name: "Sticker #6", price: 2.50, active: true
-Product.create! id: 7, name: "Sticker #7", price: 2.50, active: true
+Product.create! id: 7, name: "Sticker #7", price: 0.50, active: true
 Product.create! id: 8, name: "Sticker #8", price: 0.01, active: true
 
 OrderStatus.delete_all
