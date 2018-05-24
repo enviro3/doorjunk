0f75ed2d38598043590d5c95c4a9b54745d1c1d2
Caroline Vurlumis <cvurlumis@gmail.com>
2018-05-23 21:43:22 -0700

    Revert "Did we fix it?"
    This reverts commit 5a9f0aa7691cbd729f9a7e14a5bbf2930b982cf3.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :order_items, dependent: destroy
+  has_many :order_items
 
   default_scope { where(active: true) } #checks if active flag is set to true. Ensures that deleted/inactive products aren't shown
 end
